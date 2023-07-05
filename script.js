bd93b2e3a396cac65ef74417e8148c719c5dd8d3
lydiaqd <lydiaqdames@gmail.com>
2023-07-05 00:44:14 -0500

    New

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then(function(response) {
+       return response.json ();
+  }).then(function(response) {
+    //console.log(response);
+    // push out each astronaut to the screen
+    let myContainer = document.getElementById('container');
+
+for (let i =0; i< response.length; i++) {
+       let myHtml = `
+       <div class="astronaut">
+          <div class="bio">
+            <h3>${response[i].firstName} ${response[i].lastName}</h3>
+            <ul>
+            <li>Hours in space: ${response[i].hoursInSpace}</li>
+            <li>Active: ${response[i].active}</li>
+            <li>Skills: ${response[i].skills}</li>
+         </ul>
+     </div>
+     <img class="avatar" src="${response[i].picture}">
+</div>
+       `;
+
+       myContainer.innerHTML += myHtml;
+     }
+   })
+});
